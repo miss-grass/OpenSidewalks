4d2624eaec00f88cdec6a67d4fb1a7e44a02796a
wendoc <wendoc@cs.washington.edu>
2018-02-06 03:50:31 -0800

    finished job

--- FetchJson.py
+++ FetchJson.py
@@ -17,9 +17,22 @@ def main():
     for i in range(0, coords.shape[0]):
         origin = coords[i][0]
         dest = coords[i][1]
-        data = requests.get('https://accessmap.io/api/v2/route.json?origin=' + origin + '&destination=' + dest).json()
-        route = [item for item in data["routes"][0]["geometry"]["coordinates"]]
-        edges[i] = route
+        data = requests.get('https://accessmap.io/api/v2/route.json?maxdown=-0.1&ideal=-0.01&&maxup=0.1&origin='
+                            + origin + '&destination=' + dest + '&avoid=construction').json()
+
+        try:
+            route = [(float(item[1]), float(item[0])) for item in data["routes"][0]["geometry"]["coordinates"]]
+        except TypeError:
+            x1 = round(float(origin.split(",")[0]), 7)
+            y1 = round(float(origin.split(",")[1]), 7)
+            x2 = round(float(dest.split(",")[0]), 7)
+            y2 = round(float(dest.split(",")[1]), 7)
+            route = [(x1, y1), (x2, y2)]
+
+        # remove the duplicated points
+        rm_dup_route = list(dict.fromkeys(route))
+        edges[i] = rm_dup_route
+
         if i%100 == 0:
             print(str(i) + " steps finished!")
 
--- getData.py
+++ getData.py
@@ -1,22 +1,18 @@
 import numpy as np
-from collections import OrderedDict
 
 
 def main():
     routes = np.load('coordinates.npy')
     data = np.load('test.npy')
-    #for i in range(0, data.shape[0]):
-    for i in range(3, 4):
+    for i in range(0, data.shape[0]):
         print(len(data[i]))
         print(data[i])
-        #list(OrderedDict.fromkeys(data[i]))
-        #print(len(data[i]))
-        #print(data[i])
         print(routes[i])
         """
         for j in range(0, int(data[i].shape)):
             print(data[0][int(i*2)] + data[0][int(2*i+1)])
         """
+
 
 if __name__ == '__main__':
     main()
