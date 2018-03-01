61ca1fdf7ecdcddfb26704402e4277779fb11442
Zhu Li <liz67@cs.washington.edu>
2018-02-28 21:45:17 -0800

    0-1600 issues

--- combineIssues.py
+++ combineIssues.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 total_size = 4805
-to_csv = True
+to_csv = False
 
 
 def main():
@@ -10,10 +10,18 @@ def main():
     filename2 = 'acc_final_output/newIssues_800-1600.npy'
     data1 = np.load(filename1)
     data2 = np.load(filename2)
-    data = np.zeros(((data1.shape[0]), 1), dtype=object)
+    data = np.empty(((data1.shape[0]),), dtype=object)
+
+    for i in range(0, data1.shape[0]):
+        if len(data1[i]) == 0:
+            data[i] = data2[i]
+        elif len(data2[i]) == 0:
+            data[i] = data1[i]
+        else:
+            data[i] = data1[i] + data2[i]
 
     df = pd.DataFrame(data)
-    # print(df)
+
     start = int(filename1[27:int(filename1.index('-'))])
     end = int(filename2[(int(filename2.index('-'))+1):int(filename2.index('.'))])
 
@@ -24,9 +32,11 @@ def main():
                                     'ave_minor_issue/meter', 'elevation'])
 
     else:
-        savename = 'acc_final_output/output_' + str(start) + '-' + str(end) + '.npy'
+        savename = 'acc_final_output/newIssues_' + str(start) + '-' + str(end) + '.npy'
         np.save(savename, data)
         j = int(filename1[27:int(filename1.index('-'))])
+        print(j)
+        
         for item in data1:
             print("the " + str(j) + "th route:")
             print(item)
