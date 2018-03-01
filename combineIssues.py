55f0dfdd92d75ab74a13a4aca745ba8aa44f7a59
wendoc <wendoc@cs.washington.edu>
2018-02-28 19:48:04 -0800

    for ruby to modify

--- combineIssues.py
+++ combineIssues.py
@@ -6,29 +6,17 @@ to_csv = True
 
 
 def main():
-    filename1 = 'acc_final_output/output_0-800.npy'
-    filename2 = 'acc_final_output/output_800-1600.npy'
-    # filename3 = 'final_output/output_2500-2700.npy'
+    filename1 = 'acc_final_output/newIssues_0-800.npy'
+    filename2 = 'acc_final_output/newIssues_800-1600.npy'
     data1 = np.load(filename1)
     data2 = np.load(filename2)
-    # data3 = np.load(filename3)
-    data = np.zeros(((data1.shape[0]+data2.shape[0]), 10), dtype=object)
-    i = 0
-    for item in data1:
-        data[i] = item
-        i += 1
-    for item in data2:
-        data[i] = item
-        i += 1
-    # for item in data3:
-    #    data[i] = item
-    #    i += 1
+    data = np.zeros(((data1.shape[0]), 1), dtype=object)
+
     df = pd.DataFrame(data)
     # print(df)
-    start = int(filename1[24:int(filename1.index('-'))])
+    start = int(filename1[27:int(filename1.index('-'))])
     end = int(filename2[(int(filename2.index('-'))+1):int(filename2.index('.'))])
-    # print(start)
-    # print(end)
+
     if to_csv:
         savename = 'acc_final_output/output_' + str(start) + '-' + str(end) + '.csv'
         df.to_csv(savename, header=['starting coordinate', 'ending coordinate', 'vertices', 'issues ID',
@@ -38,7 +26,7 @@ def main():
     else:
         savename = 'acc_final_output/output_' + str(start) + '-' + str(end) + '.npy'
         np.save(savename, data)
-        j = int(filename1[24:int(filename1.index('-'))])
+        j = int(filename1[27:int(filename1.index('-'))])
         for item in data1:
             print("the " + str(j) + "th route:")
             print(item)
@@ -51,14 +39,6 @@ def main():
             print()
             print()
             j += 1
-        """
-        for item in data3:
-            print("the " + str(j) + "th route:")
-            print(item)
-            print()
-            print()
-            j += 1
-        """
 
 
 if __name__ == '__main__':
